dc113161e41768b7ca8243cff158dd1067572914
ChiefNgunjiri <h.n.g.makimei@gmail.com>
2020-10-15 11:22:10 +0200

    adding new stack method

--- repls-master/src/repls/IntREPL.scala
+++ repls-master/src/repls/IntREPL.scala
@@ -1,63 +1,46 @@
 package repls
 
+import scala.collection.mutable.Stack
+
 class Bodmas(){
     val ranking: Array[String] = Array[String]("","","^","/", "*", "+", "-")
 }
 
-class rankStack(rankList: List[Int]) {
+class CalcStack(stackCal: Stack[String]){
 
-    def this() {
-        this(List())
-    }
+    def this(){this(Stack())}
 
-    def this(rankList: List[Int], nextOp: Int){
-        this(rankList :+ nextOp)
-    }
+    def this(stackCal: Stack[String], pushValue: String){this(stackCal.push(pushValue))}
 
-    val orderedList: List[Int] =  rankList
-
-    //val bracketActive = inBracket
-
-    def getRankLast(): Int = {
-
-        return orderedList(orderedList.size - 1)
+    val orderedStack = stackCal;
 
 
-    }
-    def getRankList(): List[Int] = {
-        return orderedList
-    }
 
 }
 
-object rankStack{
-    val initList = List()
+class rankStack(rankList: List[Int]) {
+    def this() {this(List())}
+
+    def this(rankList: List[Int], nextOp: Int){this(rankList :+ nextOp)}
+
+    val orderedList: List[Int] =  rankList
+
+    def getRankLast(): Int = {return orderedList(orderedList.size - 1)}
+
+    def getRankList(): List[Int] = {return orderedList}
 }
 
 class PostFix(preList: List[String]){
 
+    def this(){this(List())}
 
-    def this(){
-        this(PostFix.init)
-
-    }
-
-    def this(preList: List[String], element: String){
-        this(preList :+ element)
-    }
+    def this(preList: List[String], element: String){this(preList :+ element)}
 
     val orderedList: List[String] = preList
 
-    def getPostFix(): List[String] ={
-        return orderedList
-    }
-
-
+    def getPostFix(): List[String] ={return orderedList}
 }
 
-object PostFix {
-    val init = List()
-}
 
 class IntREPL extends REPLBase {
     // Have a REPL of type Int
@@ -67,64 +50,88 @@ class IntREPL extends REPLBase {
     override def readEval(command: String): String = {
         val elements = command.split(" ") // split string based on whitespace
 
-        val operators: Map[String, (Int, Int) => Int] = {
-            Map(("+", _ + _),
-                ("-", _ - _),
-                ("*", _ * _),
-                ("/", _ / _))
-        }
-
         println(elements.mkString(" "))
-        return postFix(elements)
+        println(postFix(elements))
 
+        return polishCalc(postFix(elements))
 
     }
 
-    def postFix(inputArray: Array[String] ): String ={
+    def postFix(inputArray: Array[String] ): List[String] ={
         val bodmasList = new Bodmas()
         var postFixString = new PostFix()
-
-
         var rankStack = new rankStack()
+
         for(i <- 0 until inputArray.size) {
 
             if (isDouble(inputArray(i))) postFixString = new PostFix(postFixString.getPostFix(), inputArray(i))
 
             else{
-
                 val rank = bodmas(inputArray(i))
 
+                if (rank == 0 || rankStack.orderedList.size == 0) rankStack = new rankStack(rankStack.getRankList(), rank)
 
-                if (rank == 0 || rankStack.orderedList.size == 0){ rankStack = new rankStack(rankStack.getRankList(), rank); println("adding" + rank);println(rankStack.orderedList); }
                 else if (rank == 1){
-
-
                     while(rankStack.getRankLast() != 0){
-                        println(println(rankStack.getRankLast()))
                         postFixString = new PostFix(postFixString.orderedList,bodmasList.ranking(rankStack.getRankLast()))
                         rankStack = new rankStack(rankStack.getRankList().dropRight(1))
                     }
                     rankStack = new rankStack(rankStack.getRankList().dropRight(1))
-
                 }
-                else if(rank < rankStack.getRankLast()) postFixString = new PostFix(postFixString.getPostFix(), inputArray(i))
-                else {
 
+                else if(rank < rankStack.getRankLast()) postFixString = new PostFix(postFixString.getPostFix(), inputArray(i))
+
+                else {
                     rankStack = new rankStack(rankStack.getRankList(),rank)
                 }
-                println("ranklist is " + rankStack.orderedList)
             }
         }
 
         while (rankStack.orderedList.size != 0){
+
             postFixString =  new PostFix(postFixString.getPostFix(),bodmasList.ranking(rankStack.getRankLast()))
             rankStack = new rankStack(rankStack.getRankList().dropRight(1))
         }
-        return postFixString.getPostFix().mkString(" ")
+        return postFixString.getPostFix()
     }
 
-    def polishCalc(): Unit ={
-        
+    def polishCalc(polishList: List[String]): String ={
+
+        var polishStack = new CalcStack(listToStack(polishList))
+        var finalString =""
+        println(polishStack.orderedStack)
+
+        while(polishStack.orderedStack.size != 0){
+
+            val popVal1 = polishStack.orderedStack.pop()
+            polishStack = new CalcStack(polishStack.orderedStack.dropRight(1))
+            if (!polishStack.orderedStack.isEmpty){
+                val popVal2 = polishStack.orderedStack.pop()
+                polishStack = new CalcStack(polishStack.orderedStack.dropRight(1))
+                if (!polishStack.orderedStack.isEmpty) {
+
+                    val popVal3 = polishStack.orderedStack.pop()
+                    polishStack = new CalcStack(polishStack.orderedStack.dropRight(1))
+
+                    if(bodmas(popVal3) != 10){
+                        println("enterning calulation")
+                        println("solution" + operations(popVal3, popVal1, popVal1))
+                        polishStack = new CalcStack(polishStack.orderedStack, operations(popVal3, popVal1, popVal1))
+                    }
+
+                }
+                else {
+                    println("second level")
+                    finalString += (popVal1 + " " + popVal2)
+                }
+                }
+            else finalString += popVal1
+
+            println(finalString)
+
+        }
+
+        return polishStack.orderedStack.mkString(" ")
     }
 
     def isDouble(num: String): Boolean ={
@@ -141,10 +148,33 @@ class IntREPL extends REPLBase {
             case "*" => 4
             case "+" => 5
             case "-" => 6
-            case _ => 0
+            case _ => 10
         }
     }
 
+    def listToStack(list: List[String]): Stack[String] ={
+        var stackList = new CalcStack()
+        println("list size is " + list.size)
+
+        for(i <-((list.size- 1) to 0 by -1)){
+            println(stackList.orderedStack + " adiing " + list(i))
+            stackList = new CalcStack(stackList.orderedStack, list(i))
+            println(stackList.orderedStack + " added " + list(i))
+        }
+        return stackList.orderedStack
+    }
+
+    def operations(operand: String, op1: String, op2: String): String ={
+
+        if (isDouble(op1) && isDouble(op2)){
+            if (operand.equals("+"))return (op1.toInt + op2.toInt).toString
+            else if (operand.equals("-"))return (op1.toInt - op2.toInt).toString
+            else if (operand.equals("/"))return (op1.toInt / op2.toInt).toString
+            else if (operand.equals("/"))return (op1.toInt / op2.toInt).toString
+            else  return (op1.toInt / op2.toInt).toString
+        }
+        else return (op1 + " " + operand + " "+ op2 )
+    }
 
     // TODO: Implement any further functions that are specifically for an IntREPL
 }
